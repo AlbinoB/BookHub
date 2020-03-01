9c6aec321a1d1501fa1a232e156cfb0edd36df5b
AlbinoB <albinobraganza21@gmail.com>
2020-03-01 10:31:43 +0530

    recycler view ui changed

--- app/src/main/java/com/albino/bookhub/adapter/DashboardRecyclerAdapter.kt
+++ app/src/main/java/com/albino/bookhub/adapter/DashboardRecyclerAdapter.kt
@@ -1,18 +1,19 @@
 package com.albino.bookhub.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.albino.bookhub.R
-import kotlinx.android.synthetic.main.recycler_dashboard_single_row.view.*
 
 
-
-class DashboardRecyclerAdapter:RecyclerView.Adapter<DashboardRecyclerAdapter.DashboardViewHolder>() {//binds the adpater to the viewholder class
+//in the constructor val context:Context where to display it and val itemList:ArrayList<String> the data to be displayed
+class DashboardRecyclerAdapter(val context:Context,val itemList:ArrayList<String>):RecyclerView.Adapter<DashboardRecyclerAdapter.DashboardViewHolder>() {//binds the adpater to the viewholder class
 
     class DashboardViewHolder(view:View):RecyclerView.ViewHolder(view){//creating the view holder
-        val textView:TextView=view.findViewById(R.id.textViewRecyclerItem)
+        val textView:TextView=view.findViewById(R.id.textViewBookName)
     }
 
 
@@ -21,14 +22,18 @@ class DashboardRecyclerAdapter:RecyclerView.Adapter<DashboardRecyclerAdapter.Das
     //these methods are called for setting the adapter to the list
     //if we there are 100 view this method will only create the initial 10 views which are displayed and recycle them when the user scrolls
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DashboardViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view=LayoutInflater.from(parent.context).inflate(R.layout.recycler_dashboard_single_row,parent,false)//inflate the recycler view to return it
+        return DashboardViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return itemList.size//returns the size of the itemList data from the constructor
     }
 
+
+    //is used to display the right view in the right position
     override fun onBindViewHolder(holder: DashboardViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val text=itemList[position]//gets the item from the itemList sent in the constructor at the position
+        holder.textView.text=text//fill in the recieved data in the holder
     }
 }
--- app/src/main/java/com/albino/bookhub/fragment/DashboardFragment.kt
+++ app/src/main/java/com/albino/bookhub/fragment/DashboardFragment.kt
@@ -1,6 +1,7 @@
 package com.albino.bookhub.fragment
 
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,6 +10,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.albino.bookhub.R
+import com.albino.bookhub.adapter.DashboardRecyclerAdapter
 
 /**
  * A simple [Fragment] subclass.
@@ -18,8 +20,22 @@ import com.albino.bookhub.R
 
 class DashboardFragment : Fragment() {
 
-    lateinit var recyclerView:RecyclerView
+    lateinit var recyclerDashboard:RecyclerView
     lateinit var layoutManager: LinearLayoutManager
+    lateinit var recyclerAdapter:DashboardRecyclerAdapter
+
+    val bookList= arrayListOf<String>(
+        "P.S I Love You",
+        "The Great Gatsby",
+        "Madame Bovary",
+        "War and peace",
+        "Lolita",
+        "The Lord of the Ring",
+        "Madame Bovary",
+        "War and peace",
+        "Lolita",
+        "The Lord of the Ring"
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,10 +44,21 @@ class DashboardFragment : Fragment() {
 
         val view=inflater.inflate(R.layout.fragment_dashboard, container, false)
 
-        recyclerView=view.findViewById(R.id.recyclerView)//view.findViewBy() is used as this a fragment and after the layout is inflated onto the view val, we can find the id.
+        recyclerDashboard=view.findViewById(R.id.recyclerView)//view.findViewBy() is used as this a fragment and after the layout is inflated onto the view val, we can find the id.
 
         layoutManager=LinearLayoutManager(activity)//as this is a fragment we cannot pass this as the parameter.Instead we pass activity
         // Inflate the layout for this fragment
+
+        recyclerAdapter= DashboardRecyclerAdapter(activity as Context,bookList)//instance of the adapter with the context and data as parameters
+        //as is used for type casting
+
+
+        recyclerDashboard.adapter=recyclerAdapter//attaching the adapter to the respective file
+
+        recyclerDashboard.layoutManager=layoutManager//attaching the adapter to the respective file
+        //the list is setup now
+
+
         return view
     }
 
